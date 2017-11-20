0cf0b41084d29431cc1e4123abdf5cdd13c8d71b
Michael Greene <michael.greene@microsoft.com>
2017-11-20 16:17:29 -0600

    first draft adding DSC resource to manage configuration of bot host

--- PoshBot/PoshBot.psm1
+++ PoshBot/PoshBot.psm1
@@ -1,3 +1,112 @@
 
 # Track bot instnace(s) running as PS job
 $script:botTracker = @{}
+
+<#
+    DSC Resource
+#>
+[DscResource()]
+class PoshBotConfiguration
+{
+    [DscProperty(Key)]
+    [string]$Name = 'PoshBot',
+
+    [DscProperty()]
+    [string]$ConfigurationDirectory = (Join-Path -Path $env:SystemRoot -ChildPath 'poshbot'),
+    
+    [DscProperty()]
+    [string]$LogDirectory = (Join-Path -Path $env:SystemRoot -ChildPath 'poshbot'),
+
+    [DscProperty()]
+    [string]$PluginDirectory = (Join-Path -Path $env:SystemRoot -ChildPath 'poshbot'),
+
+    [DscProperty()]
+    [string[]]$PluginRepository = @('PSGallery'),
+
+    [DscProperty()]
+    [string[]]$ModuleManifestsToLoad = @(),
+
+    [DscProperty()]
+    [LogLevel]$LogLevel = [LogLevel]::Verbose,
+
+    [DscProperty()]
+    [int]$MaxLogSizeMB = 10,
+
+    [DscProperty()]
+    [int]$MaxLogsToKeep = 5,
+
+    [DscProperty()]
+    [bool]$LogCommandHistory = $true,
+
+    [DscProperty()]
+    [int]$CommandHistoryMaxLogSizeMB = 10,
+
+    [DscProperty()]
+    [int]$CommandHistoryMaxLogsToKeep = 5,
+
+    [DscProperty()]
+    [hashtable]$BackendConfiguration = @{},
+
+    [DscProperty()]
+    [hashtable]$PluginConfiguration = @{},
+
+    [DscProperty(Mandatory)]
+    [string[]]$BotAdmins = @(),
+
+    [DscProperty()]
+    [char]$CommandPrefix = '!',
+
+    [DscProperty()]
+    [string[]]$AlternateCommandPrefixes = @('poshbot'),
+
+    [DscProperty()]
+    [char[]]$AlternateCommandPrefixSeperators = @(':', ',', ';'),
+
+    [DscProperty()]
+    [string[]]$SendCommandResponseToPrivate = @(),
+
+    [DscProperty()]
+    [bool]$MuteUnknownCommand = $false,
+
+    [DscProperty()]
+    [bool]$AddCommandReactions = $true,
+
+    [DscProperty()]
+    [int]$ApprovalExpireMinutes = 30,
+
+    [DscProperty()]
+    [hashtable[]]$ApprovalCommandConfigurations = @()
+
+    <#
+        Create PoshBot configuration
+    #>
+    [void] Set()
+    {
+        $botConfiguration = New-PoshBotConfiguration @this
+        Save-PoshBotConfiguration -inputObject $botConfiguration -path (Join-Path $this.ConfigurationDirectory 'config.psd1')
+    }
+
+    <#
+        Tests if the machine contains a PoshBot configuration
+    #>
+    [bool] Test()
+    {
+        if ((Import-PowerShellDataFile -Path (Join-Path $this.ConfigurationDirectory 'config.psd1') -ErrorAction SilentlyContinue) -eq $this)
+        {
+            return $true
+        }
+        else
+        {
+            return $false
+        }
+    }
+
+    <#
+        Returns an instance of this class.
+    #>
+    [PoshBotConfiguration] Get()
+    {
+        $this = Import-PowerShellDataFile -Path (Join-Path $this.ConfigurationDirectory 'config.psd1') -ErrorAction SilentlyContinue
+        return $this
+    }
+}
