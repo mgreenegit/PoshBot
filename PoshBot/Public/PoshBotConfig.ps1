346bc6b7b94c8cf9410cd3b9ce8924b647f31888
Michael Greene <michael.greene@microsoft.com>
2017-12-13 11:17:28 -0600

    Add Config

---
+++ PoshBot/Public/PoshBotConfig.ps1
@@ -0,0 +1,59 @@
+
+<#PSScriptInfo
+
+.VERSION 0.1.0
+
+.GUID 0bf5b422-f234-4d83-89ff-8ad8f9026365
+
+.AUTHOR Michael Greene
+
+.COMPANYNAME 
+
+.COPYRIGHT 
+
+.TAGS DSCConfiguration
+
+.LICENSEURI 
+
+.PROJECTURI http://github.com/poshbot/poshbot
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+
+
+.PRIVATEDATA 
+
+#>
+
+#Requires -Module PoshBot
+
+<# 
+
+.DESCRIPTION 
+ Configures a PoshBot server 
+
+#> 
+
+Configuration PoshBot
+{
+    PoshBot BotServer
+    {
+        Ensure = 'Present'
+        Name = 'PoshBot'
+        BackEndConfiguration = @{} #TODO
+    }
+
+    Service BotService
+    {
+        Ensure = 'Present'
+        Name = 'PoshBot'
+        State = 'Running'
+    }
+}
